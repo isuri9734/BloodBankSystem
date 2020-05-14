416edf0c31fbb58f00e82f82eeb26fc52f269c3a
isuri9734 <ikmayadunne@students.nsbm.lkm>
2020-05-14 14:54:28 +0530

    add screenshots

---
+++ javascript/index.js
@@ -0,0 +1,10 @@
+
+    var ok = document.getElementById("submit");
+    var rt = document.getElementById("reset");
+
+    function myclick(){
+
+        var val1 = text.value;
+        window.alert("val1");
+    }
+
